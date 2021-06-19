77ce9387fda4b79b55be7475571fc56ce45ec122
Jared Baur <jaredbaur@fastmail.com>
2021-06-19 13:37:26 -0700

    Write function for finding closest food

--- game/logic.go
+++ game/logic.go
@@ -1,5 +1,11 @@
 package game
 
+import (
+	"errors"
+	"fmt"
+	"math"
+)
+
 func canMoveDirection(s *state, direction string) bool {
 	x := s.You.Head.X
 	y := s.You.Head.Y
@@ -25,4 +31,26 @@ func canMoveDirection(s *state, direction string) bool {
 	}
 
 	return true
+}
+
+var ErrNoFood = errors.New("no food available")
+
+func closestFood(s *state) (*coordinate, error) {
+	x := s.You.Head.X
+	y := s.You.Head.Y
+
+	if len(s.Board.Food) == 0 {
+		return nil, ErrNoFood
+	}
+
+	lowest := math.Sqrt(math.Pow(float64(s.Board.Height), 2) + math.Pow(float64(s.Board.Width), 2))
+	var lowestCoords coordinate
+
+	for _, food := range s.Board.Food {
+		if distance := math.Sqrt(math.Pow(float64(x-food.X), 2) + math.Pow(float64(y-food.Y), 2)); distance < lowest {
+			lowest = distance
+			lowestCoords = food
+		}
+	}
+	return &lowestCoords, nil
 }
--- game/logic_test.go
+++ game/logic_test.go
@@ -10,20 +10,46 @@ func TestCanMoveDirections(t *testing.T) {
 		expect    bool
 	}{
 		{
-			name:      "Empty board",
-			state:     &state{Board: board{Height: 5, Width: 5}},
+			name: "Empty board",
+			state: &state{
+				Game:  game{},
+				Turn:  0,
+				Board: board{Height: 5, Width: 5},
+				You:   snake{},
+			},
 			direction: Up,
 			expect:    true,
 		},
 		{
-			name:      "0x0 sized board",
-			state:     &state{Board: board{Height: 0, Width: 0}},
+			name: "0x0 sized board",
+			state: &state{
+				Game:  game{},
+				Turn:  0,
+				Board: board{Height: 0, Width: 0},
+				You:   snake{},
+			},
 			direction: Down,
 			expect:    false,
 		},
 		{
-			name:      "Head is under a segment of another snake",
-			state:     &state{Board: board{Height: 5, Width: 5, Snakes: []snake{{Body: []coordinate{{X: 1, Y: 1}}}}}, You: snake{Head: coordinate{X: 1, Y: 0}}},
+			name: "Head is on the left wall",
+			state: &state{
+				Game:  game{},
+				Turn:  0,
+				Board: board{Height: 5, Width: 5},
+				You:   snake{Head: coordinate{X: 0, Y: 0}},
+			},
+			direction: Left,
+			expect:    false,
+		},
+		{
+			name: "Head is under a segment of another snake",
+			state: &state{
+				Game:  game{},
+				Turn:  0,
+				Board: board{Height: 5, Width: 5, Snakes: []snake{{Body: []coordinate{{X: 1, Y: 1}}}}},
+				You:   snake{Head: coordinate{X: 1, Y: 0}},
+			},
 			direction: Up,
 			expect:    false,
 		},
@@ -31,7 +57,33 @@ func TestCanMoveDirections(t *testing.T) {
 
 	for _, tc := range tt {
 		if got := canMoveDirection(tc.state, tc.direction); got != tc.expect {
-			t.Errorf("Test %s: got: %t, expected %t", tc.name, got, tc.expect)
+			t.Errorf("Test %s: got: %t, expected %t\n", tc.name, got, tc.expect)
+		}
+	}
+}
+
+func TestClosestFood(t *testing.T) {
+	tt := []struct {
+		name   string
+		input  *state
+		expect *coordinate
+	}{
+		{
+			name:   "One food available",
+			input:  &state{Board: board{Height: 5, Width: 5, Food: []coordinate{{X: 1, Y: 1}}}, You: snake{Head: coordinate{X: 0, Y: 0}}},
+			expect: &coordinate{X: 1, Y: 1},
+		},
+		{
+			name:   "Multiple food available",
+			input:  &state{Board: board{Height: 5, Width: 5, Food: []coordinate{{X: 1, Y: 1}, {X: 4, Y: 1}}}, You: snake{Head: coordinate{X: 4, Y: 2}}},
+			expect: &coordinate{X: 4, Y: 1},
+		},
+	}
+
+	for _, tc := range tt {
+		coord, _ := closestFood(tc.input)
+		if *coord != *tc.expect {
+			t.Errorf("Test %s: got %+v, expected %+v\n", tc.name, *coord, *tc.expect)
 		}
 	}
 }
