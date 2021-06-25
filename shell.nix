d58ce816b4c89a7d825f1aae989f581150b178c9
Jared Baur <jaredbaur@fastmail.com>
2021-06-25 10:35:14 -0700

    Only get battlesnake cli when not exists

--- shell.nix
+++ shell.nix
@@ -6,7 +6,11 @@ mkShell {
   buildInputs = [go];
 
   shellHook = ''
-    go get github.com/BattlesnakeOfficial/rules/cli/battlesnake
     export PATH=$HOME/go/bin:$PATH
+
+    if ! command -v battlesnake &>/dev/null
+    then
+      go get github.com/BattlesnakeOfficial/rules/cli/battlesnake
+    fi
   '';
 }
